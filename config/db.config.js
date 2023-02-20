90ecdf5614c9e2afb60f0ef90e5c0f039cc51431
Iranks20 <rankunda48@gmail.com>
2023-02-20 13:01:57 +0300

    configured heroku database url

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,11 @@
 'use strict';
 const mysql = require('mysql');
-// local mysql db connection
+// heroku mysql db connection
 const dbConn = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'node_mysql_crud_db'
+  host     : "us-cdbr-east-06.cleardb.net",
+  user     : "b409f650b51f57",
+  password : "97683a12",
+  database : "heroku_3bb23431b16fa95"
 });
 dbConn.connect(function(err) {
   if (err) throw err;
