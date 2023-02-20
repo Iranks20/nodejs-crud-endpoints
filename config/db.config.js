899942e3a2f1990b881afd5a37c8fde2cf428772
Iranks20 <rankunda48@gmail.com>
2023-02-20 21:58:07 +0300

    heroku database

--- config/db.config.js
+++ config/db.config.js
@@ -1,16 +1,29 @@
 'use strict';
 const mysql = require('mysql');
 // heroku mysql db connection
-const dbConn = mysql.createConnection({
-  host     : "us-cdbr-east-06.cleardb.net",
-  user     : "b409f650b51f57",
-  password : "97683a12",
-  database : "heroku_3bb23431b16fa95"
+// const dbConn = mysql.createConnection({
+//   host     : "us-cdbr-east-06.cleardb.net",
+//   user     : "b409f650b51f57",
+//   password : "97683a12",
+//   database : "heroku_3bb23431b16fa95"
+// });
+// dbConn.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Database Connected!");
+// });
+
+// heroku corrections
+const mysql = require("mysql");
+const dbConfig = require("../config/db.config.js");
+
+var connection = mysql.createPool({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB
 });
-dbConn.connect(function(err) {
-  if (err) throw err;
-  console.log("Database Connected!");
-});
+
+// module.exports = connection;
 
 //ec2-user
 // const dbConn = mysql.createConnection({
