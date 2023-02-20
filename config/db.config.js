f257d2bfb2b359f5b8aca389f550b2b6716a818c
Iranks20 <rankunda48@gmail.com>
2023-02-20 23:44:29 +0300

    jjf

--- config/db.config.js
+++ config/db.config.js
@@ -1,29 +1,29 @@
 'use strict';
 const mysql = require('mysql');
 // heroku mysql db connection
-const dbConn = mysql.createPool({
-  host     : "us-cdbr-east-06.cleardb.net",
-  user     : "b409f650b51f57",
-  password : "97683a12",
-  database : "heroku_3bb23431b16fa95"
-});
-dbConn.connect(function(err) {
-  if (err) throw err;
-  console.log("Database Connected!");
-});
+// const dbConn = mysql.createConnection({
+//   host     : "us-cdbr-east-06.cleardb.net",
+//   user     : "b409f650b51f57",
+//   password : "97683a12",
+//   database : "heroku_3bb23431b16fa95"
+// });
+// dbConn.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Database Connected!");
+// });
 
 // heroku corrections
 // const mysql = require("mysql");
 // const dbConfig = require("../config/db.config.js");
 
-// var connection = mysql.createPool({
-//   host: dbConfig.HOST,
-//   user: dbConfig.USER,
-//   password: dbConfig.PASSWORD,
-//   database: dbConfig.DB
-// });
+var connection = mysql.createPool({
+  host: "us-cdbr-east-06.cleardb.net",
+  user: "b409f650b51f57",
+  password: "97683a12",
+  database: "heroku_3bb23431b16fa95"
+});
 
-// module.exports = connection;
+module.exports = connection;
 
 //ec2-user
 // const dbConn = mysql.createConnection({
@@ -36,4 +36,4 @@ dbConn.connect(function(err) {
 //   if (err) throw err;
 //   console.log("Database Connected!");
 // });
-module.exports = dbConn;
+// module.exports = dbConn;
